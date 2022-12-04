9e3b186124459911ccd850735dec225cdba18d8e
Malin18-SU <matteo.linzas@edu.cobianchi.it>
2022-12-05 00:25:11 +0100

    update

--- Siti/Progetto_Whatsapp/myScript.js
+++ Siti/Progetto_Whatsapp/myScript.js
@@ -2,78 +2,100 @@ $(document).ready(function(){
     let json_url = "chats.json"
     let Timeout = []
     let stop_chat = false
+    let stop_load = false
 
     setInterval(Time, 1000)
     load_app()
 
 
 
+    //when clicked on a chat in chats_window, it loads the chat
     $(document).on("click", "div.user > div", function(){
         $(".chats_window").hide()
-        load_chat($(this).closest(".user").attr("data-name"))
+     /*   let last
+        console.log($(this).closest(".user").attr("data-name") + ": " + $("#" + $(this).closest(".user").attr("data-name")).attr("data-lastmsg"))
+            if($("#" + $(this).closest(".user").attr("data-name")).attr("data-lastmsg") == undefined){
+                last = 0;
+            }else last = $("#" + $(this).closest(".user").attr("data-name")).attr("data-lastmsg")
+*/
+        load_chat($(this).closest(".user").attr("data-name")/*, last*/)
     })
 
+    //clear the timeout and returns to the chats_window screen
     $(document).on("click", "div .top-chat > .icon", function(){
-        $(".icon").closest(".window").remove()
+        $(".icon").closest(".window").remove()//.hide()
         stop_chat = true
         clearTimeout_msg(Timeout)
         $(".chats_window").show()
     })
 
+    //send a message from the user
     $(document).on("click", "div .send > .icon", function(){
         if($(".send-msg").val() !== "")
         send_message({
+            id: Number($(".messageBox:last").attr("id")) + 1,
             sender: "you",
             content: $(".send-msg").val(),
-            time: "[10/11/22] 10:33"
+            time: Time()
         })
         $(".send-msg").val("")
     })
 
     //load the selected chat
-    function load_chat(user){
+    function load_chat(user/*, start_msg*/){
+/*        stop_load = false
+        $(".window").each(function(i, obj){
+            if($(obj).attr("id") === user){
+                $("#" + user).show()
+                stop_load = true;
+            }
+        })
+*/
         $.getJSON(json_url, function(data){     //takes the JSON file
             data.users.forEach(function(item){      //check on every user
                 if(item.name === user){     //when finds the user's chat, it loads
-                    $(".content").append('<div class="window">' +
-                        '   <div class="top-chat d-flex flex-wrap color-secondary">' +
-                        '       <div class="icon">' +
-                        '               <img src="images/icons/back_arrow.svg">' +
-                        '       </div>' +
-                        '                    <div class="user-icon">\n' +
-                        '                        <img src=' + item.pic + '>\n' +
-                        '                    </div>\n' +
-                        '\n' +
-                        '                    <div class="mt-2 w-50">\n' +
-                        '                        <h2 class="user-name">' + item.name + '</h2>\n' +
-                        '                    </div>' +
-                        '   </div>' +
-                        ' <div class="chat"></div>' +
-                        ' <div class="bot-chat m-auto p-2 rounded d-flex flex-wrap color-main-2">' +
-                        '<input type="text" class="form-control send-msg w-75 color-main-1" placeholder="Scrivi un messaggio"</div>' +
-                        '<div class="w-25 send text-end"> ' +
-                        '<div class="icon">' +
-                        '<img src="images/icons/send.png">' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>')
+
+                   // if(!stop_load){
+                        $(".content").append('<div id ="' + user + '" class="window ' + user + '" data-lastmsg="0">' +
+                            '   <div class="top-chat d-flex flex-wrap color-secondary">' +
+                            '       <div class="icon">' +
+                            '               <img src="images/icons/back_arrow.svg">' +
+                            '       </div>' +
+                            '                    <div class="user-icon">\n' +
+                            '                        <img src=' + item.pic + '>\n' +
+                            '                    </div>\n' +
+                            '\n' +
+                            '                    <div class="mt-2 w-50">\n' +
+                            '                        <h2 class="user-name">' + item.name + '</h2>\n' +
+                            '                    </div>' +
+                            '   </div>' +
+                            ' <div class="chat"></div>' +
+                            ' <div class="bot-chat m-auto p-2 rounded d-flex flex-wrap color-main-2">' +
+                            '<input type="text" class="form-control send-msg w-75 color-main-1" placeholder="Scrivi un messaggio"</div>' +
+                            '<div class="w-25 send text-end"> ' +
+                            '<div class="icon">' +
+                            '<img src="images/icons/send.png">' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>')
+               //     }
+
                     $(".send-msg").attr('disabled', 'disabled')
-                    $(".chat").scroll()
+                    $("#" + user + " > .chat").scroll()
                     stop_chat = false
                     item.chat.every(function(msg){    //print every message
-                        Timeout[msg.id] = setTimeout(function(){
-                            if((Timeout && stop_chat) || (msg.id === item.chat[item.chat.length-1].id)){       //doesn't allow to print messages of previous chats
-                                clearTimeout_msg(Timeout)
-                                $(".send-msg").removeAttr('disabled')
-                                return false
-                            }
+                     //   if(msg.id > start_msg){
+                            Timeout[msg.id] = setTimeout(function(){
+                                if((Timeout && stop_chat) || (msg.id === item.chat[item.chat.length-1].id + 1)){       //doesn't allow to print messages of previous chats
+                                    clearTimeout_msg(Timeout)
+                                    $(".send-msg").removeAttr('disabled')
+                                    return false
+                                }
+                                $("." + user).attr("data-lastmsg", msg.id)
+                                send_message(msg)
 
-                            if(item.user === "Gabriel" && msg.id === 6){
-
-                            }
-
-                            send_message(msg)
-                        }, (msg.id-1) * 2000)
+                            }, (((msg.id-1)) /*- start_msg*/) * 2000)
+                   //     }
                         return true
                     })
                 }
@@ -82,6 +104,7 @@ $(document).ready(function(){
 
     }
 
+    //writes a message on the chat
     function send_message(msg){
         $(".chat").append('<div id="' + msg.id + '" class="messageBox bg-light">')
 
@@ -108,6 +131,7 @@ $(document).ready(function(){
 
         $(".chat").animate({
             scrollTop: $('.chat').prop("scrollHeight")}, 1000)
+
     }
 
     //load the selected chat
@@ -150,8 +174,11 @@ $(document).ready(function(){
         if(minutes < 10)
             minutes = "0" + minutes
 
+        let time = hours + ":" + minutes
 
-        $("#time").text(hours + ":" + minutes)
+        $("#time").text(time)
+
+        return "[" + d.getDate() + "/" + (d.getMonth()+1) + "/" + d.getFullYear().toString() + "]" + time
     }
 
     //when the content has a higher value than the max_char value, it shows just the ones before adding "..."
@@ -164,10 +191,4 @@ $(document).ready(function(){
 
 })
 
-
-/*
-* TO DO LIST
-*   -Funzione per stampare il contenuto in modo che dopo tot caratteri metta "..." e basta ##fatto
-*   -Funzione per visualizzare l'orario dell'ultimo messaggio sulla finestra principale ##fatto
-*   -Funzione per i messaggi
-*   -Funzione che ordina le chat in base alla data dell'ultimo messaggio*/
+/*MADE BY LINZAS MATTEO*/
