261a3d51072522869608e64c030286438c4d52ae
Malin18-SU <matteo.linzas@edu.cobianchi.it>
2022-12-28 12:37:59 +0100

    php

---
+++ Siti/PHP/compiti_vacanze_natale/es1/home.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+<html>
+    <head>
+        <title>Esercizio 1</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+    </head>
+    <body>
+
+        <form action="sendmail.php" method="post" class="m-auto d-flex flex-column w-25 gap-3 mt-3">
+            <input type="text" name="name" placeholder="nome" required class="form-control">
+            <input type="text" name="surname" placeholder="cognome" required class="form-control">
+            <input type="date" name="date" max="2022-12-12" placeholder="data di nascita" required class="form-control">
+            <input type="text" name="addr" placeholder="indirizzo" required class="form-control">
+            <input type="text" name="city" placeholder="città" required class="form-control">
+            <input type="email" name="email" placeholder="email" required class="form-control">
+
+            <input type="submit" value="Invia" class="btn btn-primary form-control">
+        </form>
+
+    </body>
+</html>
---
+++ Siti/PHP/compiti_vacanze_natale/es1/sendmail.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+ini_set('SMTP','ssl:smtp.gmail.com');
+ini_set('smtp_port',"465");
+ini_set('username', 'matteo.linzas@gmail.com');
+ini_set('password', 'AinzMatteo18');
+
+if($_POST){
+    $file = fopen("file.txt", "a+");
+
+    $_SESSION["id"] = randomPassword("user", 5);
+    $_SESSION["psw"] = randomPassword("psw");
+    $_SESSION["name"] = $_POST["name"];
+    $_SESSION["surname"] = $_POST["surname"];
+    $_SESSION["date"] = $_POST["date"];
+    $_SESSION["addr"] = $_POST["addr"];
+    $_SESSION["city"] = $_POST["city"];
+    $_SESSION["email"] = $_POST["email"];
+
+    fwrite($file, $_SESSION["name"] ."," .$_SESSION["surname"] ."," .$_SESSION["date"] ."," .$_SESSION["addr"] ."," .$_SESSION["city"] ."," .$_SESSION["email"] ."\n");
+    fclose($file);
+
+    $sender = "me";
+    $sender_mail = "mittente@weh.com";
+    $receiver = $_SESSION["email"];
+
+    $mail_obj = "boh";
+    $mail_body = "Prova";
+
+    $mail_headers = "From: " .$sender ." <" .$sender_mail . "> \r\n";
+    $mail_headers .= "Reply-To: " .$sender_mail ."> \r\n";
+    $mail_headers .= "X-Mailer: PHP/" .phpversion();
+
+    if(mail($receiver, $mail_obj, $mail_body, $mail_headers)){
+        echo "Messaggio inviato con successo\n";
+        echo "user: " .$_SESSION["id"] . " psw: " .$_SESSION["psw"] ."<br>";
+    }else echo "Errore. \n";
+
+}
+
+function randomPassword($type, $length=8) {
+    $psw = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $user = 'abcdefghijklmnopqrstuvwxyz';
+
+    if($type == "user"){
+        $alphabet = $user;
+    }else $alphabet = $psw;
+
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $length; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+?>
