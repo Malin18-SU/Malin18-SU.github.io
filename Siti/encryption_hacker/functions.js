1a1a5b382985c09243fe2c52dff9b8a6763f20fb
Malin18-SU <matteo.linzas@edu.cobianchi.it>
2022-12-31 20:25:00 +0100

    updated encryption

--- Siti/encryption_hacker/functions.js
+++ Siti/encryption_hacker/functions.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     autoplay()
     let key, text;
 
-    function autoplay(){
+    function autoplay(){    //permette di attivare l'audio su chrome            //quando gli dai il segnale fa tutto da solo, non devi fare nient'altro
         let r = confirm("Clicca 'Ok' per attivare la musica");
         if (r === true) {
             document.getElementById("player").play();
@@ -10,12 +10,10 @@ $(document).ready(function(){
     }
 
 
-
-    $("#submit").on("click", function(){
-        $(".d-error").empty()
+    $("#submit").on("click", function(){    //permette di selezionare il tipo di decodifica
         key = $("#key").val()
         text = $("#input").val()
-        switch($("#encryption").val()){
+        switch($("#encryption").val()){ //ah ok, allora questo giustifica i mezzi ch- AHAHAHAHAH, che ci sono sotto perché analizza la parola
             case "1":
                 do_DES()
                 break
@@ -41,14 +39,14 @@ $(document).ready(function(){
         }
 
 
-        function do_DES(){
+        function do_DES(){   //funzione che esegue la codifica e decodifica con DES        //*whispers* che cazzo è...
             let ciphertext = text;
 
-            if($("#mode").val() === "1"){
+            if($("#mode").val() === "1"){   //se è una codifica
                 // Encrypt
                 ciphertext = CryptoJS.DES.encrypt(text, key)
                 $("#output").val(ciphertext)
-            }else{
+            }else{      //se è una decodifica
                 // Decrypt
                 let bytes  = CryptoJS.DES.decrypt(ciphertext, key);
                 let plaintext = bytes.toString(CryptoJS.enc.Utf8);
@@ -56,7 +54,7 @@ $(document).ready(function(){
             }
         }
 
-        function do_3DES(){
+        function do_3DES(){     //funzione che esegue la codifica e decodifica con TripleDES     //*whispers ancora* la stessa cosa...
             let ciphertext = text;
 
             if($("#mode").val() === "1"){
@@ -71,7 +69,7 @@ $(document).ready(function(){
             }
         }
 
-        function do_own_3DES(){
+        function do_own_3DES(){ //funzione che esegue la codifica e decodifica con TripleDES manuale          //QUI E' il punto importante, il pilastro del codice perché qui analizza il tutto, aspet perché se non succede una cosa, succede un'altra
             key = key.split(' ');
             let ciphertext = text;
 
@@ -100,7 +98,7 @@ $(document).ready(function(){
 
         }
 
-        function do_AES(){
+        function do_AES(){   //funzione che esegue la codifica e decodifica con AES                //e in questo caso se il valore è uguale a 1, va bene, se è diverso, se ne va nelle backrooms (minchia aggiungerei)
             let ciphertext = text;
 
             if($("#mode").val() === "1"){
@@ -110,13 +108,72 @@ $(document).ready(function(){
             }else{
                 // Decrypt
                 let bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), key);
+                let plaintext = bytes.toString();
+                $("#output").val(plaintext)
+            }
+        }
+
+        function do_personal(){     //funzione che esegue la codifica e decodifica con metodo originale
+            let ciphertext = text;
+
+            if($("#mode").val() === "1"){
+                // Encrypt
+                ciphertext = own_encrypt(own_encrypt(own_encrypt(text, key), key), key)
+                $("#output").val(ciphertext)
+            }else{
+                // Decrypt
+                let bytes  = own_decrypt(own_decrypt(own_decrypt(ciphertext.toString(CryptoJS.enc.Utf8), key).toString(CryptoJS.enc.Utf8), key).toString(CryptoJS.enc.Utf8), key);
                 let plaintext = bytes.toString(CryptoJS.enc.Utf8);
                 $("#output").val(plaintext)
             }
         }
 
-        function do_personal(){
 
+        function own_encrypt(text, key){
+            let key_crypt = 0
+            let text_crypt = ""
+
+            for(let i = 0;i < key.length; i++){
+                key_crypt += key.charCodeAt(i)
+            }
+
+            console.log(key_crypt)
+            console.log(key_crypt/4)
+            console.log(key_crypt%((key_crypt/4)+1))
+            key_crypt = (key_crypt%((key_crypt/4)+1)).toString()
+
+            console.log(key_crypt)
+
+            for(let i = 0;i < text.length; i++){
+                if(i % 2 === 0){
+                    text_crypt += String.fromCharCode(text.charCodeAt(i) + (Number)(key_crypt))
+                }else text_crypt += String.fromCharCode(text.charCodeAt(i) - (Number)(key_crypt))
+
+                //console.log("text_crypt: " + text_crypt + ", text: " + text.charAt(i))
+            }
+
+            return text_crypt
+        }
+
+        function own_decrypt(text, key){
+            let key_crypt = 0
+            let text_crypt = ""
+
+            for(let i = 0;i < key.length; i++){
+                key_crypt += key.charCodeAt(i)
+            }
+
+            key_crypt = (key_crypt%((key_crypt/4)+1)).toString()
+
+            for(let i = 0;i < text.length; i++){
+                if(i % 2 === 0){
+                    text_crypt += String.fromCharCode(text.charCodeAt(i) - (Number)(key_crypt))
+                }else text_crypt += String.fromCharCode(text.charCodeAt(i) + (Number)(key_crypt))
+
+                //console.log("text_crypt: " + text_crypt + ", text: " + text.charAt(i))
+            }
+
+            return text_crypt
         }
     })
 })
